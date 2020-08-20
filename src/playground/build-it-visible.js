70c87d3254087ba500778294945a76903e2d5f74
Felipe Pineda <fpineda@live.unc.edu>
2020-08-20 11:54:51 -0400

    visibility toggle notes

---
+++ src/playground/build-it-visible.js
@@ -0,0 +1,27 @@
+let visibility = false;
+
+const toggleVisibility = () => {
+    visibility = !visibility;
+    
+    render();
+    
+}
+
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
+    const jsx = (
+        <div>
+            <h1>Visibility Toggle</h1>
+            <button onClick={toggleVisibility}>{visibility ? 'hide details': 'Show details'}</button>
+            {visibility && (
+                <p>Hey. These are some details you can now see!</p>
+            )}
+        </div>
+    );
+
+    ReactDOM.render(jsx, appRoot);
+}
+
+render();
