a7ac1b0b2d42e14977eea193b22af8a714d402c5
Felipe Pineda <fpineda@live.unc.edu>
2020-08-21 10:44:23 -0400

    more es6 class notes

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -12,7 +12,48 @@ class Person {
     }
 }
 
+class Student extends Person{
+    constructor (name, age, major) {
+        // only set major as name and age are inherited and called in by super(...)
+        super(name,age);
+        this.major = major;
+    }
 
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription(){
+        // call inherited method using super.<METHOD>() and override it
+        let description = super.getDescription();
 
-const me = new Person('Felipe Pineda', 25);
-console.log(me.getDescription());
+        if (this.hasMajor()){
+            description += ` Their major is ${this.major}.`;
+        }
+
+        return description;
+    }
+}
+
+class Traveler extends Person{
+    constructor (name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation() {
+        return !!this.homeLocation;
+    }
+
+    getGreeting(){
+        let greeting = super.getGreeting();
+
+        if (this.hasHomeLocation()) {
+            greeting += ` I'm visting from ${this.homeLocation}.`;
+        }
+
+        return greeting;
+    }
+}
+
+const me = new Traveler('Felipe Pineda', 25, 'Columbia');
+console.log(me.getGreeting());
