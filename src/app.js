e07594ef31a1106685a551e64c27b77c7d46065d
Felipe Pineda <fpineda@live.unc.edu>
2020-08-24 10:25:02 -0400

    props

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 class IndecisionApp extends React.Component {
     render() {
+        const title = 'Indecision'
+        const subtitle = 'Put your life in JavaScript\'s random functionality';
+        const options = ['Thing one', 'Thing two', 'Thing three'];
         return (
             <div>
-                <Header />
+                <Header title={title} subtitle={subtitle}/>
                 <Action />
-                <Options />
+                <Options options={options}/>
                 <AddOption />
             </div>
 
@@ -16,8 +19,8 @@ class Header extends React.Component{
     render() {
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in JavaScript's random functionality</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         );
     }
@@ -37,8 +40,8 @@ class Options extends React.Component {
     render () {
         return (
             <div>
-                <p>Options component here</p>
-                <Option />
+                {/* {this.props.options.map((option) => <p key={option}>{option}</p>)} */}
+                {this.props.options.map((option) => <Option key={option} optionText={option} />)}
             </div>
         );
     }
@@ -48,7 +51,7 @@ class Option extends React.Component {
     render() {
         return (
             <div>
-                <p>Option Component here</p>
+                {this.props.optionText}
             </div>
         )
     }
