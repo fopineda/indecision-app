d5e1f1cda217f5a5939d1f4267ce7bebaeeac804
Felipe Pineda <fpineda@live.unc.edu>
2020-09-11 15:09:38 -0400

    implemented webpack dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -15,5 +15,8 @@ module.exports = {
             exclude: /node_modules/
         }]
     },
-    devtool: 'cheap-module-eval-source-map'
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
 };
