42d734fe4acde3df94529957834cfcc887ad6629
Felipe Pineda <fpineda@live.unc.edu>
2020-09-14 14:38:13 -0400

    explicitly returning jsx instead of using the return(..)

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import Modal from 'react-modal';
 
-const OptionModal = (props) => {
-    return (
-        <Modal
-            isOpen={!!props.selectedOption}
-            contentLabel="Selected Option"
-            onRequestClose={props.handleCloseModal}
-        >
-            <h3>Selected Option</h3>
-            {props.selectedOption && <p>{props.selectedOption}</p>}
-            <button onClick={props.handleCloseModal}>X</button>
-        </Modal>
-    );
-};
+const OptionModal = (props) => (
+    <Modal
+        isOpen={!!props.selectedOption}
+        contentLabel="Selected Option"
+        onRequestClose={props.handleCloseModal}
+    >
+        <h3>Selected Option</h3>
+        {props.selectedOption && <p>{props.selectedOption}</p>}
+        <button onClick={props.handleCloseModal}>X</button>
+    </Modal>
+);
 
 export default OptionModal;
