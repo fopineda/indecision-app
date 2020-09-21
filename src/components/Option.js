11c8f725053aabe70e187c1827805a80b0617be7
Felipe Pineda <fpineda@live.unc.edu>
2020-09-21 19:12:14 -0400

    added styling to indivudal option; not done (4:00)

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Option = (props) => (
-    <div>
+    <div className="option">
         {props.optionText}
         <button 
             className='button button--link'
