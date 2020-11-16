8396f0d90ae27c643e32ec110308de09ae0042e3
Felipe Pineda <fpineda@live.unc.edu>
2020-11-16 11:19:53 -0500

    separating CSS files for production ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 // entry -> output
 
 const path = require('path')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 
 // exporting as a function so you can access the env
 module.exports = (env) => {
     const isProduction = env == 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
 
     return {
         entry: './src/app.js',
@@ -20,13 +22,17 @@ module.exports = (env) => {
                 exclude: /node_modules/
             }, {
                 test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                use: CSSExtract.extract({
+                    use: [
+                        'css-loader',
+                        'sass-loader',
+                    ]
+                })
             }]
         },
+        plugins: [
+            CSSExtract
+        ],
         devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public')
